aefaabffa6b9327365f1bc02c2365a7084e875e8
Elsa Gonsiorowski (Uranus) <me@gonsie.com>
2013-07-27 17:09:52 -0400

    echo destination when using a fishmark

--- functions/c.fish
+++ functions/c.fish
@@ -3,11 +3,13 @@ function c -d "fishmarks change to bookmark directory"
          fishmark_source
          if test -z $argv
             cd $HOME
+            echo $HOME
          else
             set -l target (eval echo \$(echo DIR_(echo $argv[1])))
             set -l warn (set_color $fish_color_error)
             if test -d $target
                cd $target
+               echo $target
             else if not test -n $target
                echo -e $warn "WARNING: $argv[1] fishmark does not exist"
             else
